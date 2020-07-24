66824339c503871e35a2bf8225ef3b1c6abf82b0
dacoonkr <dacoon0621@gmail.com>
2020-07-24 14:07:43 +0900

    >>> 없는 셸 추가

--- plim.cpp
+++ plim.cpp
@@ -2,6 +2,7 @@
 #include "concolor.h"
 #include <conio.h>
 #include <Windows.h>
+
 string codes;
 
 int main(int argc, char** argv) {
@@ -27,83 +28,84 @@ int main(int argc, char** argv) {
 		}
 	}
 	else {
-		//이전 코드
-		/*
 		string tmps;
-		cout << ">>> ";
-		while (getline(cin, tmps)) {
-			codes = tmps;
-			if (tmps.length() > 0) if (tmps[tmps.length() - 1] != ';') {
-				while (1) {
-					string tmps;
-					cout << "  > ", getline(cin, tmps);
-					codes += ";";
-					codes += tmps;
-					if (tmps.length() > 0) { if (tmps[tmps.length() - 1] == ';') break; }
-				}
-			}
-			if (codes != "") {
-				compile_and_run(codes);
-				cout << ">>> ";
-			}
-		}*/
-
-		vector<Data> tokens;
-		string now;
-		cout << ">>> ";
-		while (1) {
-			int p = _getch();
-			if (p == '\r') {
-				if (now.length() > 0) {
-					if (now[now.length() - 1] == ';') {
-						concolor(WHITE, BLACK);
-						cout << "\n";
-						compile_and_run(now);
-						_getch();
-						now = "";
-						system("cls");
-						cout << ">>> ";
-					}
-					else {
-						now += ";\n";
+		switch (false) {
+		case true:
+			while (getline(cin, tmps)) {
+				codes = tmps;
+				if (tmps.length() > 0) if (tmps[tmps.length() - 1] != ';') {
+					while (1) {
+						string tmps;
+						cout << "  > ", getline(cin, tmps);
+						codes += ";";
+						codes += tmps;
+						if (tmps.length() > 0) { if (tmps[tmps.length() - 1] == ';') break; }
 					}
 				}
+				if (codes != "") {
+					compile_and_run(codes);
+				}
 			}
-			if (p == 8) {
-				if (now.length() == 0) continue;
-				now = now.substr(0, now.length() - 1);
-			}
-			else now += (char)p;
-			tokens.clear();
-			int a = OrsplitTokens(now, &tokens);
-			system("cls");
-			concolor(WHITE, BLACK);
+			break;
+		case false:
+			vector<Data> tokens;
+			string now;
 			cout << ">>> ";
-			if (a == 1) {
-				concolor(LIGHTRED, BLACK);
-				cout << now;
-			}
-			else {
-				for (int i = 0; i < tokens.size(); i++) {
-					if (tokens[i].type == 3) {
-						concolor(BROWN, BLACK);
-						cout << "\"" << tokens[i].data << "\"";
+			while (1) {
+				int p = _getch();
+				if (p == '\r') {
+					if (now.length() > 0) {
+						if (now[now.length() - 1] == ';') {
+							concolor(WHITE, BLACK);
+							cout << "\n";
+							compile_and_run(now);
+							_getch();
+							now = "";
+							system("cls");
+							cout << ">>> ";
+						}
+						else {
+							now += ";\n";
+						}
 					}
-					else if (tokens[i].type == 1) {
-						concolor(CYAN, BLACK);
-						cout << "" << tokens[i].data << "";
-					}
-					else if (tokens[i].type == 2) {
-						if (isKeyVariable(tokens[i].data)) concolor(LIGHTMAGENTA, BLACK);
-						else concolor(LIGHTGREEN, BLACK);
-						cout << "" << tokens[i].data << "";
-					}
-					else {
-						concolor(WHITE, BLACK);
-						cout << "" << tokens[i].data << "";
+				}
+				if (p == 8) {
+					if (now.length() == 0) continue;
+					now = now.substr(0, now.length() - 1);
+				}
+				else now += (char)p;
+				tokens.clear();
+				int a = OrsplitTokens(now, &tokens);
+				system("cls");
+				concolor(WHITE, BLACK);
+				cout << ">>> ";
+				if (a == 1) {
+					concolor(LIGHTRED, BLACK);
+					cout << now;
+				}
+				else {
+					for (int i = 0; i < tokens.size(); i++) {
+						if (tokens[i].type == 3) {
+							concolor(BROWN, BLACK);
+							cout << "\"" << tokens[i].data << "\"";
+						}
+						else if (tokens[i].type == 1) {
+							concolor(CYAN, BLACK);
+							cout << "" << tokens[i].data << "";
+						}
+						else if (tokens[i].type == 2) {
+							if (isKeyVariable(tokens[i].data)) concolor(LIGHTMAGENTA, BLACK);
+							else concolor(LIGHTGREEN, BLACK);
+							cout << "" << tokens[i].data << "";
+						}
+						else {
+							concolor(WHITE, BLACK);
+							cout << "" << tokens[i].data << "";
+						}
 					}
 				}
 			}
+			break;
 		}
 	}
 }
